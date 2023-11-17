0cbab31d1ce58b3679117ba16cf86ecd7981b1b8
HamizMuaffer <hamiz.muzaffer77@gmail.com>
2023-11-17 07:25:29 +0500

    Login Password

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function showPassword(){
+    var show1 = document.getElementById("input")
+    // show1.type = "password";
+    if(show1.type === "password"){
+
+    
+show1.type = "text";}
+else{
+    show1.type = "password";
+}
+    
+}
